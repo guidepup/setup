c80a0bae20c34ba9adcff078e65c86c8b44c3505
cmorten <craig.morten@hotmail.co.uk>
2022-12-03 21:55:40 +0000

    temp: add logging

--- src/windows/setup.ts
+++ src/windows/setup.ts
@@ -8,10 +8,13 @@ const SUB_KEY_GUIDEPUP_NVDA = "HKCU\\Software\\Guidepup\\Nvda";
 export async function setup(): Promise<void> {
   // TODO: Check if have Guidepup's Portable NVDA installed
   const {
-    [SUB_KEY_GUIDEPUP_NVDA]: { exists },
+    [SUB_KEY_GUIDEPUP_NVDA]: { exists, values, keys },
   } = await regedit.list([SUB_KEY_GUIDEPUP_NVDA]);
 
+  console.log({ exists, values, keys });
+
   if (!exists) {
+    console.log("creating key");
     await regedit.createKey([SUB_KEY_GUIDEPUP_NVDA]);
   }
 
@@ -19,6 +22,7 @@ export async function setup(): Promise<void> {
 
   // TODO: Create / update registry with Guidepup's Portable NVDA installation location
   const versionedKey = `guidepup_nvda_${version}`;
+  console.log({ versionedKey });
 
   await regedit.putValue({
     [SUB_KEY_GUIDEPUP_NVDA]: {
