10cdabe019704a1a9c27e61baeb4a85d69fca64f
cmorten <craig.morten@hotmail.co.uk>
2022-12-17 22:11:34 +0000

    fix: only perform resolution fix for macos

--- .github/workflows/resolutionFix.ts
+++ .github/workflows/resolutionFix.ts
@@ -1,9 +1,11 @@
 import { execSync } from "child_process";
 
-try {
-  execSync(
-    `"/Library/Application Support/VMware Tools/vmware-resolutionSet" 1920 1080`
-  );
-} catch (_) {
-  // swallow
+if (process.platform === "darwin") {
+  try {
+    execSync(
+      `"/Library/Application Support/VMware Tools/vmware-resolutionSet" 1920 1080`
+    );
+  } catch (_) {
+    // swallow
+  }
 }
