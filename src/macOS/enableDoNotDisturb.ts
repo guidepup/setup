b8c497ebf37801bcf4b2390001fa8757467734c8
cmorten <craig.morten@hotmail.co.uk>
2023-01-25 10:16:26 +0000

    feat: add `osascript` to allowlist for controlling `com.apple.systemevents`
    Add retries to entire dnd applescript on error.

--- src/macOS/enableDoNotDisturb.ts
+++ src/macOS/enableDoNotDisturb.ts
@@ -3,12 +3,13 @@ import * as os from "os";
 import { ERR_MACOS_FAILED_TO_ENABLE_DO_NOT_DISTURB } from "../errors";
 import { runAppleScript } from "./runAppleScript";
 import { promisify } from "util";
+import { retryOnError } from "./retryOnError";
 
 // REF: https://github.com/sindresorhus/do-not-disturb/issues/9
 const enableFocusModeShellscript = `defaults write com.apple.ncprefs.plist dnd_prefs -data 62706C6973743030D60102030405060708080A08085B646E644D6972726F7265645F100F646E64446973706C6179536C6565705F101E72657065617465644661636574696D6543616C6C73427265616B73444E445875736572507265665E646E64446973706C61794C6F636B5F10136661636574696D6543616E427265616B444E44090808D30B0C0D070F1057656E61626C6564546461746556726561736F6E093341C2B41C4FC9D3891001080808152133545D6C828384858C9499A0A1AAACAD00000000000001010000000000000013000000000000000000000000000000AE && killall usernoted && killall ControlCenter`;
 
 const enableFocusModeAppleScript = `
-set timeoutSeconds to 60.0
+set timeoutSeconds to 30.0
 
 set command to "
   do shell script \\"open 'x-apple.systempreferences:com.apple.preference.notifications?focus'\\"
@@ -39,7 +40,7 @@ export async function enableDoNotDisturb() {
       await promisify(exec)(enableFocusModeShellscript);
     } else {
       // From MacOS 12 Monterey (Darwin 21) there is no known way to enable DND via system defaults
-      await runAppleScript(enableFocusModeAppleScript);
+      await retryOnError(() => runAppleScript(enableFocusModeAppleScript));
     }
   } catch (e) {
     throw new Error(`${ERR_MACOS_FAILED_TO_ENABLE_DO_NOT_DISTURB}\n${e.message}`);
