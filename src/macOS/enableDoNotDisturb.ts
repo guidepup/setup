4d361f09ce7b31f48dd9d3245d3558a3306a3b71
jlp-craigmorten <craig.morten@waitrose.co.uk>
2023-11-30 22:19:39 +0000

    fix: locale dependent naming

--- src/macOS/enableDoNotDisturb.ts
+++ src/macOS/enableDoNotDisturb.ts
@@ -8,6 +8,8 @@ import { retryOnError } from "./retryOnError";
 // REF: https://github.com/sindresorhus/do-not-disturb/issues/9
 const enableFocusModeShellscript = `defaults write com.apple.ncprefs.plist dnd_prefs -data 62706C6973743030D60102030405060708080A08085B646E644D6972726F7265645F100F646E64446973706C6179536C6565705F101E72657065617465644661636574696D6543616C6C73427265616B73444E445875736572507265665E646E64446973706C61794C6F636B5F10136661636574696D6543616E427265616B444E44090808D30B0C0D070F1057656E61626C6564546461746556726561736F6E093341C2B41C4FC9D3891001080808152133545D6C828384858C9499A0A1AAACAD00000000000001010000000000000013000000000000000000000000000000AE && killall usernoted && killall ControlCenter`;
 
+const getLocale = `defaults read -g AppleLocale`;
+
 const enableFocusModeAppleScript = `
 -- Startup delay to reduce chance of "Application isn't running (-600)" errors
 delay 1
@@ -35,24 +37,28 @@ set command to "
 my withTimeout(command, timeoutSeconds)
 `;
 
-const enableFocusModeVenturaAppleScript = `
--- Startup delay to reduce chance of "Application isn't running (-600)" errors
+const enableFocusModeVenturaAppleScript = (locale: string) => {
+  // TODO: attempt to rewrite scripts without any locale specific instructions
+  // so this setup can be used regardless of user locale preferences.
+  const center = locale.trim() === "en_GB" ? "Centre" : "Center";
+
+  return `-- Startup delay to reduce chance of "Application isn't running (-600)" errors
 delay 1
 
 set timeoutSeconds to 30.0
 
 set command to "
   tell application \\"System Events\\"
-    tell its application process \\"Control Centre\\"
-      -- Click on and open Control Centre drop down
+    tell its application process \\"Control ${center}\\"
+      -- Click on and open Control ${center} drop down
       tell its menu bar 1
-          tell (UI elements whose description is \\"Control Centre\\")
+          tell (UI elements whose description is \\"Control ${center}\\")
             click
           end tell
       end tell
 
       -- Check if Do Not Disturb already enabled
-      set doNotDisturbCheckbox to checkbox 2 of group 1 of window \\"Control Centre\\"
+      set doNotDisturbCheckbox to checkbox 2 of group 1 of window \\"Control ${center}\\"
 		  set doNotDisturbCheckboxStatus to value of doNotDisturbCheckbox as boolean
 
       tell doNotDisturbCheckbox
@@ -61,9 +67,9 @@ set command to "
         end if
       end tell
 
-      -- Click on and close Control Centre drop down
+      -- Click on and close Control ${center} drop down
       tell its menu bar 1
-          tell (UI elements whose description is \\"Control Centre\\")
+          tell (UI elements whose description is \\"Control ${center}\\")
             click
           end tell
       end tell
@@ -73,6 +79,7 @@ set command to "
 
 my withTimeout(command, timeoutSeconds)
 `;
+};
 
 export async function enableDoNotDisturb() {
   const platformMajor = Number(os.version().split("Version ")[1].split(".")[0]);
@@ -84,9 +91,11 @@ export async function enableDoNotDisturb() {
       // From MacOS 12 Monterey (Darwin 21) there is no known way to enable DND via system defaults
       await retryOnError(() => runAppleScript(enableFocusModeAppleScript));
     } else {
+      const { stdout: locale } = await promisify(exec)(getLocale);
+
       // From MacOS 13 Ventura (Darwin 22) there is no known way to enable DND via system settings
       await retryOnError(() =>
-        runAppleScript(enableFocusModeVenturaAppleScript)
+        runAppleScript(enableFocusModeVenturaAppleScript(locale))
       );
     }
   } catch (e) {
