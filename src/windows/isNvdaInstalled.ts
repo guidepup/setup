0f121fa7a703cc20dab789acc5f6ba0b2a29732f
Craig Morten <cmorten@users.noreply.github.com>
2024-01-15 10:58:22 +0000

    feat: allow custom nvda install directory (#26)

--- src/windows/isNvdaInstalled.ts
+++ src/windows/isNvdaInstalled.ts
@@ -1,14 +1,29 @@
 import { existsSync } from "fs";
 import { VERSIONED_KEY } from "./constants";
+import { RegistryItemValue } from "regedit";
 
-export function isNvdaInstalled({ exists, values }) {
+export function isNvdaInstalled({
+  exists,
+  userProvidedInstallDirectory,
+  values,
+}: {
+  exists: boolean;
+  userProvidedInstallDirectory: string | null;
+  values: {
+    [name: string]: RegistryItemValue;
+  };
+}) {
   if (!exists) {
     return false;
   }
 
-  const path = values[VERSIONED_KEY]?.value;
+  const path = values[VERSIONED_KEY]?.value as string;
 
   if (!path) {
+    return false;
+  }
+
+  if (userProvidedInstallDirectory !== path) {
     return false;
   }
 
