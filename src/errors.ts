6535af6f2233c2dfad2d24917c8c789fad084a5d
jlp-craigmorten <craig.morten@waitrose.co.uk>
2024-07-25 16:46:37 +0100

    sec: remove automation of credential injection

--- src/errors.ts
+++ src/errors.ts
@@ -13,8 +13,6 @@ export const ERR_MACOS_UNABLE_TO_WRITE_DATABASE_FILE =
   "Unable to write to the VoiceOver database file - SIP might not be disabled";
 export const ERR_MACOS_UNABLE_TO_WRITE_USER_TCC_DB =
   "Unable to write to the user TCC.db - SIP might not be disabled";
-export const ERR_MACOS_UNABLE_TO_UPDATE_UI_PREFERENCES =
-  "Unable to enable VoiceOver control via UI preferences";
 export const ERR_MACOS_UI_PROMPT_FAILURE =
   "Unable to retrieve user input from prompt";
 export const ERR_MACOS_UI_CONTROL_NOT_CONSENTED =
@@ -24,7 +22,10 @@ export const ERR_MACOS_REQUIRES_MANUAL_USER_INTERACTION =
 export const ERR_MACOS_FAILED_TO_ENABLE_DO_NOT_DISTURB =
   'Failed to enable "Do not disturb" mode';
 
-export const ERR_WINDOWS_UNABLE_TO_ACCESS_REGISTRY = "Unable to access Windows registry";
-export const ERR_WINDOWS_UNABLE_TO_UPDATE_REGISTRY = "Unable to update Windows registry";
+export const ERR_WINDOWS_UNABLE_TO_ACCESS_REGISTRY =
+  "Unable to access Windows registry";
+export const ERR_WINDOWS_UNABLE_TO_UPDATE_REGISTRY =
+  "Unable to update Windows registry";
 export const ERR_WINDOWS_FAILED_TO_INSTALL_NVDA = "Unable to install NVDA";
-export const ERR_WINDOWS_FAILED_TO_RESTART_EXPLORER = "Unable to restart explorer.exe";
+export const ERR_WINDOWS_FAILED_TO_RESTART_EXPLORER =
+  "Unable to restart explorer.exe";
