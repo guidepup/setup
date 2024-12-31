c79c4ec0a9c330914aa61d246c9c251517acbdd2
jlp-craigmorten <craig.morten@waitrose.co.uk>
2024-12-31 14:37:04 +0000

    feat: try support sequoia vo preferences being sandboxed

--- src/macOS/enableAppleScriptControlSystemDefaults.ts
+++ src/macOS/enableAppleScriptControlSystemDefaults.ts
@@ -1,12 +1,37 @@
 import { execSync } from "child_process";
+import { getPlatformVersionMajor } from "./getPlatformVersionMajor";
 import { ERR_MACOS_UNABLE_UPDATE_SYSTEM_DEFAULTS } from "../errors";
 
+const VOICE_OVER_APPLESCRIPT_ENABLED_DEFAULTS =
+  "defaults write com.apple.VoiceOver4/default SCREnableAppleScript -bool true";
+
+const VOICE_OVER_APPLESCRIPT_ENABLED_PLUTIL =
+  "plutil -replace SCREnableAppleScript -bool true ~/Library/Group\\ Containers/group.com.apple.VoiceOver/Library/Preferences/com.apple.VoiceOver4/default.plist";
+
 export function enableAppleScriptControlSystemDefaults(): void {
+  const platformMajor = getPlatformVersionMajor();
+
+  // For MacOS 14 Sonoma (Darwin 23) and earlier VoiceOver preferences are set via system defaults.
+  if (platformMajor < 24) {
+    try {
+      execSync(VOICE_OVER_APPLESCRIPT_ENABLED_DEFAULTS);
+
+      return;
+    } catch (e) {
+      throw new Error(
+        `${ERR_MACOS_UNABLE_UPDATE_SYSTEM_DEFAULTS}\n\n${e.message}`
+      );
+    }
+  }
+
+  // From MacOS 15 Sequoia (Darwin 24) VoiceOver preferences are sandboxed.
   try {
-    execSync(
-      "defaults write com.apple.VoiceOver4/default SCREnableAppleScript -bool true"
-    );
+    execSync(VOICE_OVER_APPLESCRIPT_ENABLED_PLUTIL);
+
+    return;
   } catch (e) {
-    throw new Error(`${ERR_MACOS_UNABLE_UPDATE_SYSTEM_DEFAULTS}\n\n${e.message}`);
+    throw new Error(
+      `${ERR_MACOS_UNABLE_UPDATE_SYSTEM_DEFAULTS}\n\n${e.message}`
+    );
   }
 }
