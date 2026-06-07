62c3b5a7f81858898a80a6d87add54af899c3216
cmorten <craig.morten@hotmail.co.uk>
2026-06-07 15:24:01 +0100

    fix: notification center is not bootout safe

--- src/macOS/disableNotificationCenter.ts
+++ src/macOS/disableNotificationCenter.ts
@@ -3,10 +3,7 @@ import { ERR_MACOS_UNABLE_DISABLE_NOTIFICATION_CENTER } from "../errors";
 
 export function disableNotificationCenter(): void {
   try {
-    execSync(
-      "launchctl bootout gui/$(id -u) /System/Library/LaunchAgents/com.apple.notificationcenterui.plist",
-      { encoding: "utf8" },
-    );
+    execSync("killall NotificationCenter", { encoding: "utf8" });
   } catch (e) {
     throw new Error(
       `${ERR_MACOS_UNABLE_DISABLE_NOTIFICATION_CENTER}\n\n${e.message}`,
