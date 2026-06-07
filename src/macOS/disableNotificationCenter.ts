e9561a54b46d6ebd8a4fc3364d93527e8b55787a
cmorten <craig.morten@hotmail.co.uk>
2026-06-07 15:09:27 +0100

    fix: move away from unload

--- src/macOS/disableNotificationCenter.ts
+++ src/macOS/disableNotificationCenter.ts
@@ -4,7 +4,7 @@ import { ERR_MACOS_UNABLE_DISABLE_NOTIFICATION_CENTER } from "../errors";
 export function disableNotificationCenter(): void {
   try {
     execSync(
-      "launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist",
+      "launchctl bootout /System/Library/LaunchAgents/com.apple.notificationcenterui.plist",
       { encoding: "utf8" },
     );
   } catch (e) {
