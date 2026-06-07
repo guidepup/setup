b962bc32ba64aaea3e2e80b781ede41846c73004
cmorten <craig.morten@hotmail.co.uk>
2026-06-07 15:12:55 +0100

    fix: provide id

--- src/macOS/disableNotificationCenter.ts
+++ src/macOS/disableNotificationCenter.ts
@@ -4,7 +4,7 @@ import { ERR_MACOS_UNABLE_DISABLE_NOTIFICATION_CENTER } from "../errors";
 export function disableNotificationCenter(): void {
   try {
     execSync(
-      "launchctl bootout /System/Library/LaunchAgents/com.apple.notificationcenterui.plist",
+      "launchctl bootout gui/$(id -u) /System/Library/LaunchAgents/com.apple.notificationcenterui.plist",
       { encoding: "utf8" },
     );
   } catch (e) {
