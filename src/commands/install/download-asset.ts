e714f59e8555513c1ee22c12a51f8fbd7487fdfb
cmorten <craig.morten@hotmail.co.uk>
2026-07-17 21:10:47 +0100

    feat: support proxy env vars

--- src/commands/install/download-asset.ts
+++ src/commands/install/download-asset.ts
@@ -1,14 +1,19 @@
 import { createWriteStream } from "node:fs";
 import { pipeline } from "node:stream/promises";
+import { EnvHttpProxyAgent, fetch } from "undici";
 import type { Asset } from "./types";
 import { ERR_INSTALL_FAILED_TO_DOWNLOAD_ASSET } from "../../errors";
+
+const dispatcher = new EnvHttpProxyAgent();
 
 export async function downloadAsset(
   asset: Asset,
   source: string,
   destination: string,
 ): Promise<void> {
-  const response = await fetch(source);
+  const response = await fetch(source, {
+    dispatcher,
+  });
 
   if (!response.ok || !response.body) {
     throw new Error(
